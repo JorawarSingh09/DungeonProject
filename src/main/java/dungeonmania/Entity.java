d0346b9b573a55f4cbc2e5c0338b9d6d7d1948e9
jora_mb <jora090901@gmail.com>
2022-07-05 14:52:49 +1000

    created entity methods and attributes, we need to go over EntityType next meeting

--- src/main/java/dungeonmania/Entity.java
+++ src/main/java/dungeonmania/Entity.java
@@ -1,5 +1,59 @@
 package dungeonmania;
 
 public class Entity {
-    
+    // EntityType type;
+    int id;
+    int xPos;
+    int yPos;
+    boolean interactable;
+    boolean collidable;
+
+    public Entity(int id, int xPos, int yPos, bool interactable, boolean collidable){
+        this.id = id;
+        this.xPos = xPos;
+        this.yPos = yPos; 
+        this.interactable = interactable;
+        this.collidable = collidable;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getXPos() {
+        return this.xPos;
+    }
+
+    public void setXPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getYPos() {
+        return this.yPos;
+    }
+
+    public void setYPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public boolean isInteractable() {
+        return this.interactable;
+    }
+
+    public void setInteractable(boolean interactable) {
+        this.interactable = interactable;
+    }
+
+    public boolean isCollidable() {
+        return this.collidable;
+    }
+
+    public void setCollidable(boolean collidable) {
+        this.collidable = collidable;
+    }
+
 }
