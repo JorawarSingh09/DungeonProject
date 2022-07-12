92b36297f66f74787fc65b37bfb640d0d8fab058
z5363458 <z5363458@ad.unsw.edu.au>
2022-07-12 23:15:14 +1000

    add items to inventory

--- src/main/java/dungeonmania/battles/Battle.java
+++ src/main/java/dungeonmania/battles/Battle.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dungeonmania.entities.movingentities.Player;
+import dungeonmania.entities.movingentities.properties.Inventory;
 import dungeonmania.interfaces.Health;
 
 public class Battle {
+
     List<Round> rounds = new ArrayList<>();
     Health enemy;
     Player player;
+    Inventory playerInventory;
 
-    public Battle(Health enemy, Player player) {
+    public Battle(Health enemy, Player player, Inventory playerInventory) {
         this.enemy = enemy;
         this.player = player;
+        this.playerInventory = playerInventory;
     }
 
     public void startBattle() {
         while (!gameOver()) {
             startRound();
         }
-
     }
 
     public void startRound() {
--- src/main/java/dungeonmania/entities/movingentities/properties/Inventory.java
+++ src/main/java/dungeonmania/entities/movingentities/properties/Inventory.java
@@ -3,12 +3,20 @@ package dungeonmania.entities.movingentities.properties;
 import java.util.ArrayList;
 import java.util.List;
 
+import dungeonmania.interfaces.Attacking;
 import dungeonmania.interfaces.Buildable;
+import dungeonmania.interfaces.Defending;
+import dungeonmania.interfaces.Durability;
 import dungeonmania.interfaces.Storeable;
 
 public class Inventory {
+
     List<Storeable> inventoryItems = new ArrayList<>();
     List<Buildable> buildableItems = new ArrayList<>();
+    List<Attacking> attackingItems = new ArrayList<>();
+    List<Defending> defendingItems = new ArrayList<>();
+    List<Durability> durabilityItems = new ArrayList<>();
+
 
     public Inventory(List<Storeable> inventoryItems) {
         this.inventoryItems = inventoryItems;
